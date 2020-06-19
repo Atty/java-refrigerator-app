ee5bfafcb0b4eed550eb6b2f119350293c4640d9
Atty <eugenetkachevs@gmail.com>
2020-06-19 23:57:13 +0800

    Update (added soft filter)

--- src/main/java/refrigerationApp/repos/SearchRepo.java
+++ src/main/java/refrigerationApp/repos/SearchRepo.java
@@ -5,32 +5,39 @@ import refrigerationApp.store.ProductStore;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class SearchRepo {
 	
-	private static final String            URL = "jdbc:postgresql://90.188.48.32:5432/Refregerator";
-	private static       ArrayList<String> ingredientsList;
-	private static       ArrayList<String> idList;
-	private static       Connection        connection;
+	private static final String       URL = "jdbc:postgresql://90.188.48.32:5432/Refregerator";
+	private static       List<String> ingredientsList;
+	private static       List<String> copyOfIngredientsList;
+	private static       List<String> idList;
+	private static       List<String> copyOfIdList;
+	private static       Connection   connection;
 	
 	private static void postgresDBToArray() throws SQLException {
 		connection = DriverManager.getConnection(URL, "postgres", "123");
 		Statement statement     = connection.createStatement();
 		ResultSet columnsFromDB = statement.executeQuery("SELECT * from recipe");
-		idList          = new ArrayList<>();
-		ingredientsList = new ArrayList<>();
+		idList                = new ArrayList<>();
+		copyOfIdList          = new ArrayList<>();
+		ingredientsList       = new ArrayList<>();
+		copyOfIngredientsList = new ArrayList<>();
 		while (columnsFromDB.next()) {
 			idList.add(columnsFromDB.getString("id").toLowerCase().trim());
 			ingredientsList.add(columnsFromDB.getString("ingredients").toLowerCase().trim());
 		}
+		copyOfIdList.addAll(idList);
+		copyOfIngredientsList.addAll(ingredientsList);
 	}
 	
-	public static void searchPositionOfRecipeHardSearch() throws SQLException {
+	public static void searchPositionOfRecipeHardFilter() throws SQLException {
 		postgresDBToArray();
 		for (Product product : ProductStore.getProducts()) {
 			String productTitle = product.getProductTitle();
 			for (int i = 0; i < ingredientsList.size(); i++) {
-				ingredientsList.set(i, ingredientsList.get(i).replace(productTitle, ""));
+				ingredientsList.set(i, ingredientsList.get(i).replace(productTitle, "").trim());
 			}
 		}
 		for (int i = 0; i < ingredientsList.size(); i++) {
@@ -41,7 +48,21 @@ public class SearchRepo {
 		idList.removeIf(n -> n.equals("-1"));
 	}
 	
-	public static ArrayList<String> getIdList() {
+	public static void searchPositionOfRecipeSoftFilter() {
+		for (int i = 0; i < ingredientsList.size(); i++) {
+			if (ingredientsList.get(i).matches("(.+(\\s\\S).+){2,}") ||
+			    ingredientsList.get(i).equals(copyOfIngredientsList.get(i))) {
+				copyOfIdList.set(i, "-1");
+			}
+		}
+		copyOfIdList.removeIf(n -> n.equals("-1"));
+	}
+	
+	public static List<String> getIdList() {
 		return idList;
+	}
+	
+	public static List<String> getCopyOfIdList() {
+		return copyOfIdList;
 	}
 }
